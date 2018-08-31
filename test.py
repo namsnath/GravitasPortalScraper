972d9b8b63ab7cde0c4478e92cd1aba8f2212be2
Namit Nathwani <namsnath@gmail.com>
2018-09-01 01:11:39 +0530

    More test updates

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import json
+import sys
+from bs4 import BeautifulSoup
+import re
 
-
+print('Starting Script')
 opts = Options()
 opts.add_argument('log-level=3')
+opts.add_argument('--no-sandbox')
 opts.add_argument('--headless')
 opts.add_argument('--disable-gpu') 
 browser = Chrome(options=opts)
+print('Browser running, getting page')
+
 
 browser.get('http://info.vit.ac.in/gravitas18/gravitas/gravitas_coordinator_login.asp')
-wait = WebDriverWait(browser, 100)
+wait = WebDriverWait(browser, 10)
 
 src = browser.page_source
+print(src)
 
 myElem = wait.until(EC.visibility_of_element_located((By.ID, 'loginid')))
 
